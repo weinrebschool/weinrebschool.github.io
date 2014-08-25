8f70d1cfe6cd5c5bd44f7c602617a5ebabd9c1ee
Sahibjot Saggu <sahibjot@live.ca>
2014-08-25 16:17:09 -0400

    added pictures to slider

--- script.js
+++ script.js
@@ -9,5 +9,16 @@ Filename: script.js
  */
 
  $(document).ready(function() {
- 	
+ 	// change slideshow text line-height
+ 	var slide_height = $("#pic1").css("height");
+ 	$("#pic1").css("line-height", slide_height);
+ 	$("#pic2").css("line-height", slide_height);
+
+
+ 	// on right arrow click
+
+ 	$("#right_arrow").click(function() {
+ 		document.write("Right arrow clicked.");
+ 	});
+
  });

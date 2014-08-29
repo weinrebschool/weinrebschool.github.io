f356c0a098078e4badc7a1fedb523194642f29ed
Sahibjot Saggu <sahibjot@live.ca>
2014-08-28 20:47:46 -0400

    adjusting stuff on "our program" page

--- our_program_script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	//$("#phone_footer_2").css("top")
-	var cont_h = Number(($("#content").css("height")).replace("px", ""));
-	var tot = toString(cont_h + 40);
-	$("#phone_footer_2").css("top", '1080px');
-	//document.write($("#phone_footer_2").css("top"));
-});
